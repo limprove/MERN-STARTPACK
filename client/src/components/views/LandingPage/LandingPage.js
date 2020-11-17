d4d69d68dd5a2662429114a9d92d75e0e0db1ca9
limprove <limprove89@gmail.com>
2020-11-17 16:41:51 +0900

    start pack

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Icon} from 'antd'
+
+function LandingPage() {
+    return (
+        <>
+            <div className="app">
+            <span style={{ fontSize: '5rem'}}><Icon type="smile" /></span>
+            </div>
+        </>
+    )
+}
+
+export default LandingPage
